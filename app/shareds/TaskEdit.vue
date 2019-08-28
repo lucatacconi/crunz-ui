4083c1432a6f6583f10effaae793b9512f6ce9b9
Luca Tacconi <luca.tacconi@monolite.net>
2019-08-28 09:29:57 +0200

    Modal  edit  for task management - part 1

---
+++ app/shareds/TaskEdit.vue
@@ -0,0 +1,27 @@
+<template id="taskEdit" lang="html">
+    <div>
+        <v-card>
+
+            <v-card-text class="pt-5">
+            </v-card-text>
+
+        </v-card>
+    </div>
+</template>
+
+<script type="text/javascript">
+    module.exports = {
+        data: function() {
+            return {
+
+            }
+        },
+        props: [],
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+</style>
