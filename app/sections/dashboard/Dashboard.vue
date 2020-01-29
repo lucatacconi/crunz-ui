87ef9a6a5ab848b65e3840b97b4e2a74bba74bb8
Luca Tacconi <luca.tacconi@monolite.net>
2020-01-29 18:40:15 +0100

    Dashboard area for graphs

--- app/sections/dashboard/Dashboard.vue
+++ app/sections/dashboard/Dashboard.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <v-container>
-            <v-row v-if="env_check.ALL_CHECK">
-                <v-col md="6" xs="12" class="mt-0 ml-0 pl-0 pt-0">
+            <v-row v-if="env_check.ALL_CHECK" class="pa-0">
+                <v-col md="6" sm="12" class="pl-md-0 pr-md-1 mx-sm-0 px-sm-0">
                     <v-card>
                         <v-card-title>Daily task's prospect</v-card-title>
                         <v-card-text>
-                            <canvas id="graph-area-1"></canvas>
+                            <canvas id="graph-area-1" height="100"></canvas>
                         </v-card-text>
                     </v-card>
                 </v-col>
-                <v-col md="6" xs="12" class="mt-0 mr-0 pr-0 pt-0">
+                <v-col md="6" sm="12" class="pr-md-0 pl-md-1 mx-sm-0 px-sm-0">
                     <v-card>
-                        <v-card-title>Prova1</v-card-title>
+                        <v-card-title>Weekly task's prospect</v-card-title>
                         <v-card-text>
-                            <canvas id="graph-area-2"></canvas>
+                            <canvas id="graph-area-2" height="100"></canvas>
                         </v-card-text>
                     </v-card>
                 </v-col>
@@ -192,8 +192,15 @@
     module.exports = {
         data: function() {
             return {
-                env_check: {},
-                message: ""
+                "env_check": {},
+                "message": "",
+                "stats": {
+                    "daily":{
+                        "planned": 0,
+                        "executed": 0,
+                        "failed": 0
+                    }
+                }
             }
         },
         methods: {
@@ -214,18 +221,79 @@
                         }
 
                         if(self.env_check.ALL_CHECK){
+
+
                             setTimeout(
                                 function(){
-                                    self.graphDaily();
-                                    self.graphWeekly();
+                                    self.stat();
+                                    // self.graphWeekly();
                                 }, 200
                             );
+
+
+
+
+                            // setTimeout(
+                            //     function(){
+                            //         self.graphDaily();
+                            //         self.graphWeekly();
+                            //     }, 200
+                            // );
                         }
 
                     }else{
                         self.message = "Error reading environment check data";
                     }
                 });
+            },
+
+            stat:function(){
+                var self = this;
+                var params = {
+                    "return_task_cont": "N",
+                    "calc_run_lst": "Y",
+                    "show_past_planned_task": "Y",
+                    "interval_from": moment().subtract(3, 'days').format("YYYY-MM-DD"),
+                    "interval_to": moment().add(3, 'days').format("YYYY-MM-DD")
+                }
+
+                Utils.apiCall("get", "/task/",params)
+                .then(function (response) {
+                    if(response.data.length != 0){
+                        for (i = 0; i < response.data.length; i++) {
+                            task_data = response.data[i];
+
+                            console.log(JSON.stringify(task_data));
+
+                            for (var task_data_start in task_data.interval_run_lst) {
+                                if(task_data_start.substring(0, 10) == moment().format("YYYY-MM-DD")){
+                                    self.stats.daily.planned += 1;
+                                }
+                            }
+
+                            for (var task_data_exec in task_data.executed_task_lst) {
+                                if(task_data_exec.substring(0, 10) == moment().format("YYYY-MM-DD")){
+                                    self.stats.daily.executed += 1;
+                                }
+                            }
+                        }
+
+                    }else{
+
+                    }
+
+
+
+
+                    console.log(JSON.stringify(self.stats));
+
+
+
+                    // console.log(JSON.stringify(response));
+                    //  console.log(JSON.stringify(params));
+                });
+
+
             },
 
             graphDaily:function(){
