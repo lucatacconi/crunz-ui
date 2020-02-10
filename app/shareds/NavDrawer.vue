3de0d13962cccee9b8e6764dbc8e41b2e7518cfc
Luca Tacconi <luca.tacconi@monolite.net>
2020-02-10 19:48:30 +0100

    Fix notice clicking on same nav menu item

--- app/shareds/NavDrawer.vue
+++ app/shareds/NavDrawer.vue
@@ -99,48 +99,42 @@ module.exports = {
 
 
     props:['drawer','selection', 'navmap'],
-    watch: {
-        // selection:function(){
-        //     for(var i=0;i<this.items.length;i++){
-        //         if(i==this.selection){
-        //             this.items[i].color="blue"
-        //         }else{
-        //             this.items[i].color="black"
-        //         }
-        //     }
-        // }
-    },
     methods: {
         launchEvent:function(navItem){
-            sessionStorage.setItem("activeSection", navItem.title);
-            this.$emit('selectedsection', navItem.title);
 
-            if(navItem.actionType=="LINK"){
-                window.open(navItem.action.url, navItem.action.target)
-            }else if(navItem.actionType=="SECT"){
-                //Rimuovo colore da tutte le voci
-                for(var i=0;i<this.navmap.length;i++){
-                    if(this.navmap[i].layout!=undefined){
-                        this.navmap[i].layout.color=false
-                    }
-                    if(this.navmap[i].subMenuItems!=undefined){
-                        for(var k=0;k<this.navmap[i].subMenuItems.length;k++){
-                            if(this.navmap[i].subMenuItems[k].layout!=undefined){
-                                this.navmap[i].subMenuItems[k].layout.color=false
+            if( sessionStorage.getItem("activeSection") != '' && sessionStorage.getItem("activeSection") != navItem.title ){
+
+                sessionStorage.setItem("activeSection", navItem.title);
+                this.$emit('selectedsection', navItem.title);
+
+                if(navItem.actionType=="LINK"){
+                    window.open(navItem.action.url, navItem.action.target)
+                }else if(navItem.actionType=="SECT"){
+                    //Rimuovo colore da tutte le voci
+                    for(var i=0;i<this.navmap.length;i++){
+                        if(this.navmap[i].layout!=undefined){
+                            this.navmap[i].layout.color=false
+                        }
+                        if(this.navmap[i].subMenuItems!=undefined){
+                            for(var k=0;k<this.navmap[i].subMenuItems.length;k++){
+                                if(this.navmap[i].subMenuItems[k].layout!=undefined){
+                                    this.navmap[i].subMenuItems[k].layout.color=false
+                                }
                             }
                         }
                     }
+                    //Coloro la voce corrente
+                    if(navItem.layout!=undefined){
+                        navItem.layout.color="red--text"
+                    }
+                    router.push(navItem.action.path);
+                }else if(navItem.actionType=="FUNC"){
+                    var F = new Function (navItem.action);
+                    return(F())
+                }else{
+                    return;
                 }
-                //Coloro la voce corrente
-                if(navItem.layout!=undefined){
-                    navItem.layout.color="red--text"
-                }
-                router.push(navItem.action.path);
-            }else if(navItem.actionType=="FUNC"){
-                var F = new Function (navItem.action);
-                return(F())
-            }else{
-                return;
+
             }
         }
     },
