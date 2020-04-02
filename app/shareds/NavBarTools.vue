cda9b4afa01b140d43b93106659eccba97e94049
Luca Tacconi <luca.tacconi@monolite.net>
2020-04-02 02:37:37 +0200

    Code cleaning

--- app/shareds/NavBarTools.vue
+++ app/shareds/NavBarTools.vue
@@ -1,11 +1,6 @@
 <template id="navbartools" lang="html">
     <span>
-        <!-- <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-        </v-btn>
-        <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-        </v-btn> -->
+
     </span>
 </template>
 
