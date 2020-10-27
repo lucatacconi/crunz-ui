d8f3796997f8b8507b4bf6b60b12a3908e8be624
Luca Tacconi <luca.tacconi@monolite.net>
2020-10-27 12:46:03 +0100

    Recursive task's tree reader

--- routes/api/task-container.php
+++ routes/api/task-container.php
@@ -40,6 +40,105 @@ $app->group('/task-container', function (RouteCollectorProxy $group) {
         $TASKS_DIR = $crunz_base_dir . "/" . ltrim($crunz_config["source"], "/");
 
         $data = CrunzUITools::taskDirectoryRotator($TASKS_DIR);
+        sort($data);
+
+        $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        return $response->withStatus(200)
+                        ->withHeader("Content-Type", "application/json");
+    });
+
+    $group->get('/tree/display', function (Request $request, Response $response, array $args) {
+
+        $data = [];
+
+        $params = array_change_key_case($request->getQueryParams(), CASE_UPPER);
+
+
+        $app_configs = $this->get('configs')["app_configs"];
+        $base_path =$app_configs["paths"]["base_path"];
+
+        if(empty($_ENV["CRUNZ_BASE_DIR"])){
+            $crunz_base_dir = $base_path;
+        }else{
+            $crunz_base_dir = $_ENV["CRUNZ_BASE_DIR"];
+        }
+
+        if(!file_exists ( $crunz_base_dir."/crunz.yml" )) throw new Exception("ERROR - Crunz.yml configuration file not found");
+        $crunz_config_yml = file_get_contents($crunz_base_dir."/crunz.yml");
+
+        if(empty($crunz_config_yml)) throw new Exception("ERROR - Crunz configuration file empty");
+
+        try {
+            $crunz_config = Yaml::parse($crunz_config_yml);
+        } catch (ParseException $exception) {
+            throw new Exception("ERROR - Crunz configuration file error");
+        }
+
+        if(empty($crunz_config["source"])) throw new Exception("ERROR - Tasks directory configuration empty");
+
+        $TASKS_DIR = $crunz_base_dir . "/" . ltrim($crunz_config["source"], "/");
+
+        $aTASKS_DIR = CrunzUITools::taskDirectoryRotator($TASKS_DIR);
+        sort($aTASKS_DIR);
+
+        $data = [];
+        $data = [ 'subdir' => '/', 'description' => 'Main task', 'children' => [] ];
+
+
+        foreach($aTASKS_DIR as $aTASKS_DIR_cnt => $tasks_dir){
+
+            if(empty($tasks_dir)){
+                continue;
+            }
+
+            $aPATH_dir = explode("/", $tasks_dir);
+
+            if(empty($aPATH_dir[1])){
+                continue;
+            }
+
+            $depth = count($aPATH_dir) - 1;
+
+            $section_ref = &$data["children"];
+            $path = '';
+            $descr = '';
+
+            foreach($aPATH_dir as $path_cnt => $path_data){
+
+                if($path_cnt == 0){
+                    continue;
+                }
+
+                $child_founded =false;
+                $child_key_ref = -1;
+
+                $path .= '/'. $path_data;
+                $descr .= ucfirst($path_data) . ' - ';
+
+                foreach($section_ref as $child_key => $child_data){
+                    if($child_data["subdir"] == $path){
+                        $child_founded = true;
+                        $child_key_ref = $child_key;
+                        break;
+                    }
+                }
+
+                if($child_founded){
+                    $section_ref =  &$section_ref[$child_key]["children"];
+                }else{
+                    $row = [];
+                    $row["subdir"] = $path;
+                    $row["description"] = rtrim($descr, ' - ');
+
+                    if($path_cnt < $depth){
+                        $row["children"] = [];
+                    }
+
+
+                    $section_ref[] = $row;
+                }
+            }
+        }
 
         $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
         return $response->withStatus(200)
