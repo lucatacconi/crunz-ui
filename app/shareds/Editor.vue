0b733d17400661d864aaaa3b7fb001e2b9381adb
Emanuele <m.emanuele96@gmail.com>
2020-11-23 15:54:27 +0100

    Modify crunz action buttons

--- app/shareds/Editor.vue
+++ app/shareds/Editor.vue
@@ -8,6 +8,40 @@
             tile
         >
             <v-toolbar-title>Task content</v-toolbar-title>
+            <v-btn
+                class="ml-2 pa-0"
+                small
+                dense
+                dark
+                color="blue darken-2"
+                @click="undo()"
+            >
+                <v-icon>mdi-undo</v-icon>
+                undo
+            </v-btn>
+            <v-btn
+                class="ml-2 pa-0"
+                small
+                dense
+                dark
+                color="blue darken-2"
+                @click="redo()"
+            >
+                <v-icon>mdi-redo</v-icon>
+                redo
+            </v-btn>
+            <v-btn
+                class="ml-2 pa-0"
+                small
+                dense
+                dark
+                color="blue darken-2"
+                @click="show_crunz_button=!show_crunz_button"
+                v-if="actionButton!=undefined"
+            >
+                <v-icon>{{show_crunz_button ? 'mdi-eye-off' : 'mdi-eye'}}</v-icon>
+                {{show_crunz_button ? 'Hide crunz button' : 'Show crunz button'}}
+            </v-btn>
             <v-spacer></v-spacer>
             <v-btn
                 icon
@@ -21,25 +55,50 @@
             v-if="actionButton!=undefined"
             class="pb-2"
         >
-            <v-btn
-                class="ml-2 pa-0"
-                dark
-                small
-                dense
-                color="blue darken-2"
-                @click="test()"
+            <!-- <v-layout
+                row
+                wrap
+                class="ma-0"
             >
-                Daily
-            </v-btn>
-            <v-btn
-                class="ml-2 pa-0"
-                dark
-                small
-                dense
-                color="blue darken-2"
-            >
-                Monthly
-            </v-btn>
+                <v-flex xs6>
+                    <v-select
+                        class="pa-0 ml-2"
+                        hide-details
+                        outlined
+                        dense
+                        :items="[]"
+                    ></v-select>
+                </v-flex>
+                <v-flex xs4
+                    class="ml-2"
+                    style="margin-top:1px;"
+                >
+                    <v-btn
+                        dark
+                        color="blue darken-2"
+                    >
+                        Insert
+                    </v-btn>
+                </v-flex>
+            </v-layout> -->
+            <center v-if="show_crunz_button">
+                <template
+                    v-for="(item,i) in crunz_button"
+                >
+                    <v-btn
+                        :key="i"
+                        class="mt-2 ml-2 pa-0"
+                        dark
+                        small
+                        dense
+                        color="blue darken-2"
+                        style="width:160px;"
+                        @click="test()"
+                    >
+                        {{item.text}}
+                    </v-btn>
+                </template>
+            </center>
         </div>
 
         <v-card-text class="pa-0">
@@ -53,6 +112,93 @@ module.exports = {
     data:function(){
         return{
             taskEditEditor: null,
+            show_crunz_button:false,
+            crunz_button:[
+                {
+                    text:"hourly"
+                },
+                {
+                    text:"daily"
+                },
+                {
+                    text:"on"
+                },
+                {
+                    text:"at"
+                },
+                {
+                    text:"dailyAt"
+                },
+                {
+                    text:"between"
+                },
+                {
+                    text:"from"
+                },
+                {
+                    text:"to"
+                },
+                {
+                    text:"twiceDaily"
+                },
+                {
+                    text:"weekdays"
+                },
+                {
+                    text:"mondays"
+                },
+                {
+                    text:"tuesdays"
+                },
+                {
+                    text:"wednesdays"
+                },
+                {
+                    text:"thursdays"
+                },
+                {
+                    text:"fridays"
+                },
+                {
+                    text:"saturdays"
+                },
+                {
+                    text:"sundays"
+                },
+                {
+                    text:"weeklyOn"
+                },
+                {
+                    text:"monthly"
+                },
+                {
+                    text:"quarterly"
+                },
+                {
+                    text:"yearly"
+                },
+                {
+                    text:"days"
+                },
+                {
+                    text:"hour"
+                },
+                {
+                    text:"minute"
+                },
+                {
+                    text:"dayOfMonth"
+                },
+                {
+                    text:"month"
+                },
+                {
+                    text:"dayOfWeek"
+                },
+                {
+                    text:"preventOverlapping"
+                }
+            ],
         }
     },
     props:['content','actionButton'],
@@ -67,6 +213,7 @@ module.exports = {
 
             ed = ace.edit(editor);
             ed.getSession().setMode("ace/mode/php");
+            ed.getSession().setUndoManager(new ace.UndoManager())
 
             ed.setOptions({
                 showPrintMargin: false,
@@ -97,7 +244,13 @@ module.exports = {
         test:function(){
             var cursor=this.taskEditEditor.selection.getCursor()
             this.taskEditEditor.getSession().getDocument().insertInLine(cursor,"prova")
-        }
+        },
+        redo:function(){
+            this.taskEditEditor.redo()
+        },
+        undo:function(){
+            this.taskEditEditor.undo()
+        },
     },
     mounted:function() {
         var self=this
