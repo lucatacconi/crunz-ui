46a1e9a827c1c580d9f502c7f71d54fb24c35c08
Emanuele Marchesotti <emanuele.marchesotti@in4matic.it>
2021-11-18 11:44:13 +0100

    Update navDrawer  to vue3-sfc-loader

--- app/shareds/NavDrawer.vue
+++ app/shareds/NavDrawer.vue
@@ -1,88 +1,86 @@
 <template>
-    <div>
-        <v-navigation-drawer
-            v-model="drawer"
-            app
-            mobile-breakpoint="sm"
-        >
-            <v-list dense>
-                <template v-for="navItem in navMap">
-                    <v-list-group
-                        v-if="navItem.type == 'SUBM'"
-                        :value="navItem.layout.expanded ? navItem.layout.expanded : null"
-                        :key="navItem.id"
-                        active-class="groupActivated-NavDrawer"
-                    >
-                        <template v-slot:activator>
-                            <v-list-item-icon>
-                                <v-icon>{{ navItem.layout.icon }}</v-icon>
-                            </v-list-item-icon>
-
-                            <v-list-item-content>
-                                <v-list-item-title>
-                                    {{ navItem.title }}
-                                </v-list-item-title>
-                                <v-list-item-subtitle v-if="navItem.subtitle">
-                                    {{ navItem.subtitle }}
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </template>
-
-                        <v-list-item
-                            dense
-                            v-for="subItem in navItem.subMenuItems"
-                            :key="subItem.title"
-                            @click="launchEvent(subItem)"
-                            :disabled="subItem.layout.disabled || navItem.layout.disabled "
-                            :class="subItem.layout.class ? subItem.layout.class : null"
-                            active-class="itemActivated"
-                        >
-                            <v-list-item-icon>
-                                <v-icon>{{ subItem.layout.icon }}</v-icon>
-                            </v-list-item-icon>
-
-                            <v-list-item-content>
-                                <v-list-item-title
-                                    :class="subItem.layout.color ? subItem.layout.color : (subItem.action.path == activeRoute ? 'red--text' : null) "
-                                >
-                                    {{ subItem.title }}
-                                </v-list-item-title>
-                                <v-list-item-subtitle v-if="subItem.subtitle">
-                                    {{ subItem.subtitle }}
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-group>
-
-                    <v-divider v-if="navItem.type == 'DIV'" :key="navItem.id"></v-divider>
-
-                    <v-list-item
-                        dense
-                        v-else-if="navItem.type == 'ELM'"
-                        :key="navItem.id"
-                        @click="launchEvent(navItem)"
-                        :disabled="navItem.layout.disabled"
-                        :class="navItem.layout.class ? navItem.layout.class : null"
-                    >
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        mobile-breakpoint="sm"
+    >
+        <v-list dense>
+            <template v-for="navItem in navMap">
+                <v-list-group
+                    v-if="navItem.type == 'SUBM'"
+                    :value="navItem.layout.expanded ? navItem.layout.expanded : null"
+                    :key="navItem.id"
+                    active-class="groupActivated-NavDrawer"
+                >
+                    <template v-slot:activator>
                         <v-list-item-icon>
                             <v-icon>{{ navItem.layout.icon }}</v-icon>
                         </v-list-item-icon>
 
                         <v-list-item-content>
-                            <v-list-item-title
-                                :class="navItem.layout.color ? navItem.layout.color : (navItem.action.path == activeRoute ? 'red--text' : null)"
-                            >
+                            <v-list-item-title>
                                 {{ navItem.title }}
                             </v-list-item-title>
                             <v-list-item-subtitle v-if="navItem.subtitle">
                                 {{ navItem.subtitle }}
                             </v-list-item-subtitle>
                         </v-list-item-content>
+                    </template>
+
+                    <v-list-item
+                        dense
+                        v-for="subItem in navItem.subMenuItems"
+                        :key="subItem.title"
+                        @click="launchEvent(subItem)"
+                        :disabled="subItem.layout.disabled || navItem.layout.disabled "
+                        :class="subItem.layout.class ? subItem.layout.class : null"
+                        active-class="itemActivated"
+                    >
+                        <v-list-item-icon>
+                            <v-icon>{{ subItem.layout.icon }}</v-icon>
+                        </v-list-item-icon>
+
+                        <v-list-item-content>
+                            <v-list-item-title
+                                :class="subItem.layout.color ? subItem.layout.color : (subItem.action.path == activeRoute ? 'red--text' : null) "
+                            >
+                                {{ subItem.title }}
+                            </v-list-item-title>
+                            <v-list-item-subtitle v-if="subItem.subtitle">
+                                {{ subItem.subtitle }}
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
                     </v-list-item>
-                </template>
-            </v-list>
-        </v-navigation-drawer>
-    <div>
+                </v-list-group>
+
+                <v-divider v-if="navItem.type == 'DIV'" :key="navItem.id"></v-divider>
+
+                <v-list-item
+                    dense
+                    v-else-if="navItem.type == 'ELM'"
+                    :key="navItem.id"
+                    @click="launchEvent(navItem)"
+                    :disabled="navItem.layout.disabled"
+                    :class="navItem.layout.class ? navItem.layout.class : null"
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ navItem.layout.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title
+                            :class="navItem.layout.color ? navItem.layout.color : (navItem.action.path == activeRoute ? 'red--text' : null)"
+                        >
+                            {{ navItem.title }}
+                        </v-list-item-title>
+                        <v-list-item-subtitle v-if="navItem.subtitle">
+                            {{ navItem.subtitle }}
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script>
