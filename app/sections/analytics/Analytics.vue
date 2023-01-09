e2c89e3393335995228e68f0514e719e3fd0ee67
Luca Tacconi <luca.tacconi@monolite.net>
2023-01-09 13:24:52 +0100

    Empty section creation for Analytics

---
+++ app/sections/analytics/Analytics.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <v-card class="mb-16">
+            <v-card-title >
+                Analytics
+            </v-card-title>
+        </v-card>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data:function(){
+        return{
+            task_path_lovs:[]
+        }
+    },
+    methods: {
+        readData:function(options = {}){
+            var self = this;
+        },
+    },
+
+    computed: {
+
+    },
+
+    created:function() {
+        // this.readData();
+    },
+
+    mounted:function(){
+        // this.readData();
+    },
+
+    components:{ }
+}
+</script>
