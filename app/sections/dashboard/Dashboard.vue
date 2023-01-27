218a9b6c86ea36e041027acf903c097ec27498e4
Luca Tacconi <luca.tacconi@monolite.net>
2023-01-27 17:57:30 +0100

    Typo in component filename

--- app/sections/dashboard/Dashboard.vue
+++ app/sections/dashboard/Dashboard.vue
@@ -26,7 +26,7 @@
         },
 
         components: {
-            'dailygraph': httpVueLoader('../../../app/sections/dashboard/DaylyGraph.vue' + '?v=' + new Date().getTime()),
+            'dailygraph': httpVueLoader('../../../app/sections/dashboard/DailyGraph.vue' + '?v=' + new Date().getTime()),
             'weeklygraph': httpVueLoader('../../../app/sections/dashboard/WeeklyGraph.vue' + '?v=' + new Date().getTime()),
             'environmentcheck': httpVueLoader('../../../app/sections/dashboard/EnvironmentCheck.vue' + '?v=' + new Date().getTime())
         }
